fc7b4fa27b6116736e60433ab429fa27fee7bb61
lilfuz <lilfuz@gmail.com>
2024-03-06 19:21:40 +0300

    I won.

--- firstbot.py
+++ firstbot.py
@@ -26,32 +26,43 @@ def clear_machine():
             temp_user_data.get_all_temp_data()[i][0] = 0
         time.sleep(10)
 
+def create_new_post(message):
+    user_id = message.from_user.id
+    posts[user_id] = message.text
+    bot.reply_to(message, "Пост создан. Чтобы выполнить рассылку, введите /sendpost")
+
 def is_admin(user_id, admin_ids):
     return user_id in admin_ids
 
 
 def main():
-    @bot.message_handler(commands=['start'])
+    @bot.message_handler(commands=['start', 'admin', 'sendpost'])
     def start_msg(message):
-        buttons = Bot_inline_btns_first()
-        user_name = message.from_user.first_name
-        bot.send_message(message.chat.id, f"Привет, {user_name}, я бот для администрирования группы.\nID чата: {message.chat.id}",
-                         reply_markup=buttons.start_btns())
-
-    @bot.message_handler(commands=['admin'])
-    def admin_msg(message):
-        buttons = Bot_inline_btns_first()
         user_id = message.from_user.id
         user_name = message.from_user.first_name
-        if is_admin(message.chat.id, admin_ids):
-            bot.send_message(message.chat.id, f'Привет!, {user_name}!', reply_markup=buttons.admin_btns())
-        else:
-            bot.send_message(message.chat.id, 'У вас нет прав на эту команду!')
+        command = message.text.replace('/', '')
+        buttons = Bot_inline_btns_first()
+        if command == 'start':
+            user_name = message.from_user.first_name
+            bot.send_message(message.chat.id, f"Привет, {user_name}, я бот для администрирования группы.\nID чата: {message.chat.id}",
+                             reply_markup=buttons.start_btns())
+        elif is_admin(user_id, admin_ids):
+            if command == 'admin':
+                bot.send_message(message.chat.id, f'Привет!, {user_name}!', reply_markup=buttons.admin_btns())
+            elif command == 'sendpost':
+                data = db_actions.get_random_post()
+                if data is not None:
+                    bot.send_photo(chat_id=config.get_config()['chat_id'], caption=data[1], photo=data[0])
+                else:
+                    bot.send_message(user_id, text='Сначала создайте пост /admin')
 
-    @bot.message_handler(content_types=['text'])
+    @bot.message_handler(content_types=['text', 'photo'])
     def text(message):
+        print(temp_user_data.get_all_temp_data())
         user_input = message.text
+        photo = message.photo
         user_id = message.from_user.id
+        print(user_id)
         temp_user_data.temp_data(user_id)[user_id][0] += 1
         if temp_user_data.temp_data(user_id)[user_id][0] >= 10:
             temp_user_data.temp_data(user_id)[user_id][1] += 1
@@ -63,67 +74,45 @@ def main():
                 bot.restrict_chat_member(chat_id=config.get_config()['chat_id'], user_id=user_id)
                 bot.send_message(chat_id=config.get_config()['chat_id'], text='Вы были забанены за спам!')
                 temp_user_data.reset_user(user_id)
-        print(message.text) # работает и дальше ошибка
-        if temp_user_data.temp_data(user_id)[user_id][3] == 1:
-            print(321)
-            print(temp_user_data.temp_data(message.chat.id)[message.chat.id][3][0])
-            temp_user_data.temp_data(message.chat.id)[message.chat.id][3][0] = user_input
-            temp_user_data.temp_data(user_id)[user_id][3] = None
-        elif temp_user_data.temp_data(user_id)[user_id][3] == 2:
-            temp_user_data.temp_data(message.chat.id)[message.chat.id][3][1] = user_input
-            bot.send_message(message.chat.id, 'Введите контент поста')
-            temp_user_data.temp_data(user_id)[user_id][3] = 3
-        elif temp_user_data.temp_data(user_id)[user_id][3] == 3:
-            temp_user_data.temp_data(message.chat.id)[message.chat.id][3][2] = user_input
-            temp_user_data.temp_data(user_id)[user_id][3] = None
-            db_actions.add_post(temp_user_data.temp_data(user_id)[user_id][2:4])
-
-    @bot.message_handler(func=lambda message: True)
-    def handle_message(message):
-        user_id = message.from_user.id
-        current_time = time.time()
-
-        # Проверка на спам: если пользователь отправляет сообщения слишком быстро, считаем его сообщения спамом
-        if user_id in last_message_times and current_time - last_message_times[user_id] < 5:
-            bot.reply_to(message, "Пожалуйста, перестаньте отправлять сообщения так быстро.")
-        else:
-            # Если сообщение не является спамом, обработка сообщения по умолчанию
-            # (короче тут это  - эхо-ответ на сообщение пользователя)
-            last_message_times[user_id] = current_time  # Обновляем время последнего сообщения пользователя
-            bot.reply_to(message, message.text)
-
-    # команда /newpost для создания нового поста
-    @bot.message_handler(commands=['newpost'])
-    def handle_newpost(message):
-        bot.reply_to(message, "Введите текст нового поста:")
-        bot.register_next_step_handler(message, create_new_post)
-
-    # Создание нового поста
-    def create_new_post(message):
-        user_id = message.from_user.id
-        posts[user_id] = message.text
-        bot.reply_to(message, "Пост создан. Чтобы выполнить рассылку, введите /sendpost")
-
-    # команда /sendpost для рассылки созданного поста в группу
-    @bot.message_handler(commands=['sendpost'])
-    def handle_sendpost(message):
-        user_id = message.from_user.id
-        if user_id in posts:
-            post_text = posts[user_id]
-            bot.send_message(message.chat_id, post_text)
-            bot.reply_to(message, "Пост успешно отправлен в группу.")
-        else:
-            bot.reply_to(message, "Сначала создайте новый пост с помощью команды /newpost")
+        if temp_user_data.temp_data(user_id)[user_id][2] == 1:
+            if user_input is not None:
+                db_actions.del_post(user_input)
+                temp_user_data.temp_data(user_id)[user_id][2] = None
+                bot.send_message(message.chat.id, 'Пост удалён')
+            else:
+                bot.send_message(message.chat.id, 'Это не текст')
+        elif temp_user_data.temp_data(user_id)[user_id][2] == 2:
+            if photo is not None:
+                photo_forward_id = message.id
+                temp_user_data.temp_data(user_id)[user_id][3][1] = photo_forward_id
+                photo_id = photo[-1].file_id
+                photo_file = bot.get_file(photo_id)
+                photo_bytes = bot.download_file(photo_file.file_path)
+                temp_user_data.temp_data(user_id)[user_id][3][1] = photo_bytes
+                temp_user_data.temp_data(user_id)[user_id][2] = 3
+                bot.send_message(message.chat.id, 'Введите контент поста')
+            else:
+                bot.send_message(message.chat.id, 'Это не фото')
+        elif temp_user_data.temp_data(user_id)[user_id][2] == 3:
+            if user_input is not None:
+                temp_user_data.temp_data(user_id)[user_id][3][2] = user_input
+                temp_user_data.temp_data(user_id)[user_id][2] = None
+                db_actions.add_post(temp_user_data.temp_data(user_id)[user_id][3][1], temp_user_data.temp_data(user_id)[user_id][3][2])
+                bot.send_message(message.chat.id, 'Пост добавлен')
+            else:
+                bot.send_message(message.chat.id, 'Это не текст')
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback(call):
         user_id = call.message.chat.id
         if call.data == 'create_post':
             bot.send_message(call.message.chat.id, 'Отправьте фото поста')
-            temp_user_data.temp_data(user_id)[user_id][3] = 2
+            temp_user_data.temp_data(user_id)[user_id][2] = 2
         elif call.data == 'delete_post':
+            for i in db_actions.get_all_posts():
+                bot.send_photo(chat_id=call.message.chat.id, caption=f'ID поста: {i[0]}\nОписание: {i[2]}', photo=i[1])
             bot.send_message(call.message.chat.id, 'Введите ID поста: ')
-            temp_user_data.temp_data(user_id)[user_id][3] = 1
+            temp_user_data.temp_data(user_id)[user_id][2] = 1
 
     bot.polling(none_stop=True)
 
